e05b044204ec07fcc862db3e7770eee1df4951e1
xtlr212 <168139318+xtlr212@users.noreply.github.com>
2024-04-25 17:18:43 -0500

    Update open_html.py

--- open_html.py
+++ open_html.py
@@ -5,8 +5,8 @@ def open_html_with_about_blank(html_file_path):
     webbrowser.open_new_tab(url)
 
     # You can add your HTML file path here to open it in a separate tab
-    webbrowser.open_new_tab("file://" + chiefkeefunblockerV1.html)
+    webbrowser.open_new_tab("file://"ChiefkeefunblockerV1-main\chiefkeefunblockerV1.html
 
 if __name__ == "__main__":
-    html_file_path = "chiefkeefunblockerV1.html"
+    html_file_path = "ChiefkeefunblockerV1-main\chiefkeefunblockerV1.html"
     open_html_with_about_blank(html_file_path)
